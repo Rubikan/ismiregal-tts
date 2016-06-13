ad5cc888d2cdb72cdbfd51664b3ba6d6645d2893
Rubikan <rubikandreas@gmail.com>
2016-06-13 16:09:54 +0200

    added logging to voice utilities

--- src/main/java/at/rubikan/ismiregal/util/VoiceCache.java
+++ src/main/java/at/rubikan/ismiregal/util/VoiceCache.java
@@ -4,6 +4,7 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.RemovalListener;
 import com.google.common.cache.RemovalNotification;
+import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -12,13 +13,19 @@ import java.util.concurrent.TimeUnit;
  * @author Andreas Rubik
  */
 public class VoiceCache {
+    private static Logger log = Logger.getLogger(VoiceCache.class);
     private static Cache<String, File> cache = CacheBuilder.newBuilder()
             .maximumSize(10000)
             .expireAfterAccess(10, TimeUnit.MINUTES)
             .removalListener(new RemovalListener<String, File>() {
                 public void onRemoval(RemovalNotification<String, File> removal) {
                     if (removal.getValue() != null)
-                        removal.getValue().delete();
+                        if (removal.getValue().delete()) {
+                            log.info("The file " + removal.getValue().getName() + " has been deleted from the cache!");
+                        } else {
+                            log.warn("Error removing the file " + removal.getValue().getName() + " from the cache!");
+                        }
+
                 }
             })
             .build();
--- src/main/java/at/rubikan/ismiregal/util/VoiceUtilities.java
+++ src/main/java/at/rubikan/ismiregal/util/VoiceUtilities.java
@@ -5,6 +5,7 @@ import marytts.MaryInterface;
 import marytts.exceptions.MaryConfigurationException;
 import marytts.exceptions.SynthesisException;
 import marytts.util.data.audio.MaryAudioUtils;
+import org.apache.log4j.Logger;
 
 import javax.sound.sampled.AudioInputStream;
 import java.io.File;
@@ -21,6 +22,17 @@ public class VoiceUtilities {
     private static final String IST_MIR_EGAL = " ist mir egal";
     private static VoiceCache vc = new VoiceCache();
     private static MaryInterface marytts;
+    private static Logger log = Logger.getLogger(VoiceUtilities.class);
+
+    public VoiceUtilities() {
+        try {
+            marytts = new LocalMaryInterface();
+            marytts.setLocale(Locale.GERMAN);
+            marytts.setVoice("bits3-hsmm");
+        } catch (Exception e) {
+            log.error("Error initializing LocalMaryInterface!");
+        }
+    }
 
     public File getWav(String text) throws MaryConfigurationException, SynthesisException, IOException {
         if (vc.get(text) != null) {
@@ -32,9 +44,6 @@ public class VoiceUtilities {
     }
 
     private File generateWav(String text) throws MaryConfigurationException, SynthesisException, IOException {
-        marytts = new LocalMaryInterface();
-        marytts.setLocale(Locale.GERMAN);
-        marytts.setVoice("bits3-hsmm");
         String path = System.getProperty("java.io.tmpdir") + File.separator + text.replace(" ", "") + ".wav";
         AudioInputStream audio = marytts.generateAudio(text);
         double[] samples = MaryAudioUtils.getSamplesAsDoubleArray(audio);
@@ -44,7 +53,6 @@ public class VoiceUtilities {
     }
 
     public List<String> getVoices() throws MaryConfigurationException {
-        marytts = new LocalMaryInterface();
         Set<String> voices = marytts.getAvailableVoices();
         List<String> list = new ArrayList<>();
         list.addAll(voices);
