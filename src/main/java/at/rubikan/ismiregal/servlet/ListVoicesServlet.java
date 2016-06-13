b06c178c9b4386c753545cbcf8264f82c6510759
Rubikan <rubikandreas@gmail.com>
2016-06-13 19:54:14 +0200

    added ListVoicesServlet

---
+++ src/main/java/at/rubikan/ismiregal/servlet/ListVoicesServlet.java
@@ -0,0 +1,38 @@
+package at.rubikan.ismiregal.servlet;
+
+import at.rubikan.ismiregal.util.VoiceUtilities;
+import com.google.gson.Gson;
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Andreas Rubik
+ */
+@WebServlet("/services/listVoices")
+public class ListVoicesServlet extends HttpServlet {
+    private static VoiceUtilities vu = new VoiceUtilities();
+    private static Logger log = Logger.getLogger(ListVoicesServlet.class);
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            List<String> voices = vu.getVoices();
+            log.trace(voices);
+            String json = new Gson().toJson(voices);
+
+            response.setContentType("application/json");
+            response.getWriter().print(json);
+        } catch (Exception e) {
+            log.error("Error getting voices!", e);
+            response.getWriter().print("ERROR");
+        }
+    }
+}
